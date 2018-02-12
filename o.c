b3ad20e7b1167c0957c566d58a11d7c323139d86
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-12 13:46:47 +0530

    Create o.c

---
+++ o.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char a[30],b[30];
+printf("enter the first string");
+scanf("%S",a);
+printf("enter the second string");
+scanf("%s",b);
+printf("%s%s",a,b);
+return 0;
